2caf3f96856d9167d63a8eb4c4a2e3bc9cd64dbf
Andy Briggs <andybriggs@ANBR00460.local>
2025-04-14 17:44:44 +0100

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,10 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      colors: {
-        primary: '#ff9776',
-      },
       fontFamily: {
         sans: ['sans-serif'],
-      },
-      backgroundColor: {
-        'light': '#f1f5f9',
-        'dark': '#0d0950',
       },
     },
   },
   plugins: [],
-  darkMode: 'class',
 }
