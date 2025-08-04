390875e43adab0fcc25f52bc3cdcd799d7ca4ece
Andy Briggs <andybriggs@ANBR00460.local>
2025-08-04 19:13:07 +0100

    Added SEO tags and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  site: 'https://curiouscoders.co.uk/',
+  integrations: [sitemap()]
 });
